610c4e3a09f03465ff26db15b0bc11a765a40ab2
Shaik Gousiya <gousiya611shaik@gmail.com>
2023-07-11 21:33:02 +0530

    automaticconversion

---
+++ automaticconversion.java
@@ -0,0 +1,9 @@
+public class automaticconversion {
+     public static void main(String[] args) {
+        int i= 23456788;
+        int l=i+2;
+        System.out.println(l);
+        
+    }
+    
+}
