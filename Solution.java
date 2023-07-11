9769d0255d644ec3b2e5df9d9fdf1f45e9579aa5
Shaik Gousiya <gousiya611shaik@gmail.com>
2023-07-11 21:55:42 +0530

    rectangulararea

---
+++ Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * Solution
+ */
+public class Solution {	
+	public static void main(String args[]) {
+		int l,b;
+		Scanner scan = new Scanner(System.in);
+		l=scan.nextInt();
+		b=scan.nextInt();
+		System.out.println(l*b);
+		
+		// Write code here
+		
+	}
+}
